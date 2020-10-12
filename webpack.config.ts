2fd949b56099be90655e1ad0dd6122479dab0ab9
Ilya Malanin <loremipsum.inside@gmail.com>
2020-10-12 18:55:03 +0300

    chore(build): Add dependencies and pipeline configuration

---
+++ webpack.config.ts
@@ -0,0 +1,63 @@
+import path from 'path';
+import {CleanWebpackPlugin} from 'clean-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import {ProvidePlugin} from 'webpack';
+
+module.exports = {
+  target: 'web',
+  entry: './src/index',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: `[name].js`,
+    chunkFilename: '[id].[chunkhash].js',
+  },
+  resolve: {
+    extensions: ['.wasm', '.ts', '.tsx', '.mjs', '.cjs', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{from: 'assets', globOptions: {ignore: '**/index.html'}}],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Match-three game with ECS',
+      template: 'assets/index.html',
+    }),
+    new ProvidePlugin({
+      PIXI: 'pixi.js',
+    }),
+  ],
+  devServer: {
+    inline: true,
+    port: 9000,
+    compress: true,
+    watchContentBase: true,
+    publicPath: '/',
+    contentBase: [path.join(__dirname, 'dist')],
+    hot: true,
+  },
+  optimization: {
+    chunkIds: 'named',
+    mergeDuplicateChunks: true,
+    removeEmptyChunks: true,
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+};
